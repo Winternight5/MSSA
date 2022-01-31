3a6fecd5d12784b629b1f5d563dc9656fb3450ad
Winternight5 <54605837+Winternight5@users.noreply.github.com>
2022-01-31 11:24:51 -0800

    Add files via upload

---
+++ Week4/Assignment4.1/Helper.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Assignment4._1
+{
+    /* Menu Object with callback function.
+     * Use with Helper Class -> MainMenu(MenuItem[]);
+     * (For reuse on console applications)
+     */
+    class MenuItem
+    {
+        public string name { get; }
+        public Action callback { get; }
+
+        public MenuItem(string name, Action callback) {
+            this.name = name;
+            this.callback = callback;
+        }
+
+        public void run() {
+            callback();
+        }
+    }
+    class Helper
+    { /* Helper Class: 
+       * (For reuse on console applications)
+       *    Coloring function
+       *    Menu item selection/validation
+       *    Sanitize input types
+       *    Printing console text with color
+       */
+        private bool activeMenu = true;
+        public string[] ColorsName = new string[7] { "red", "blue", "yellow", "cyan", "magenta", "green", "gray" };
+
+        public ConsoleColor Colors(int index) {
+            switch (index) {
+                case 0:
+                    return ConsoleColor.Red;
+
+                case 1:
+                    return ConsoleColor.Blue;
+
+                case 2:
+                    return ConsoleColor.Yellow;
+
+                case 3:
+                    return ConsoleColor.Cyan;
+
+                case 4:
+                    return ConsoleColor.Green;
+
+                default:
+                    return ConsoleColor.Gray;
+
+            }
+        }
+
+        public void MainMenu(MenuItem[] menuOptions, int startOption = 0) {
+            char choice = 'Y';
+            int option = startOption;
+
+            do {
+            start:
+                if (option == -1) {
+                    ConsoleWriteLineWithColor("Available programs:", ConsoleColor.DarkBlue);
+                    ConsoleWriteLineWithColor(string.Join("\n", menuOptions.Select((t, i) => String.Format($"{i + 1} = {t.name}"))));
+
+                    do {
+                        option = InputInteger("Enter a program number to run: ") - 1;
+
+                    } while (option >= menuOptions.Length || option < 0);
+
+                    Console.WriteLine();
+                    menuOptions[option].run();
+
+                } else
+                    menuOptions[option].run();
+
+                if (!activeMenu)
+                    goto exitProgram;
+
+                tryAgain:
+                Console.Write("\nDo you want to try again? Y=Yes, X=Exit, any key=No: ");
+
+                try {
+                    choice = Char.ToUpper(Char.Parse(Console.ReadLine()));
+
+                } catch (Exception ex) {
+                    ConsoleWriteLineWithColor(ex.Message, ConsoleColor.Red);
+                    goto tryAgain;
+                }
+
+                if (choice == 'X') {
+                    activeMenu = false;
+
+                } else if (choice != 'Y') {
+                    ConsoleWriteLineWithColor($"Exiting \"{menuOptions[option].name}\" program.\n", ConsoleColor.Magenta);
+                    option = -1;
+                    goto start;
+                }
+
+            exitProgram:
+                if (!activeMenu)
+                    ConsoleWriteLineWithColor($"Good bye.", ConsoleColor.Magenta);
+
+            } while (activeMenu);
+
+            Console.ReadLine();
+        }
+
+        public void ExitProgram() {
+            activeMenu = false;
+        }
+
+        public int ValidateMenuOption(string[] arr, string inputLabel) {
+            int option;
+            do {
+                option = InputInteger($"{inputLabel} {string.Join(", ", arr.Select((t, i) => String.Format($"{i}={t}")))}: ");
+
+            } while (option >= arr.Length || option < 0);
+
+            return option;
+        }
+
+        public int InputInteger(String inputLabel = "", string inputErrorMsg = "Your input is not in a correct format.") {
+        tryAgain:
+            // print label of the input
+            Console.Write(inputLabel);
+            int inputValue = 0;
+
+            try {
+                inputValue = Int32.Parse(Console.ReadLine());
+
+            } catch (Exception ex) {
+                ConsoleWriteLineWithColor(inputErrorMsg, ConsoleColor.Red);
+                goto tryAgain;
+            }
+
+            return inputValue;
+        }
+
+        public float InputFloat(String inputLabel = "", string inputErrorMsg = "Your input is not in a correct format.") {
+        tryAgain:
+            // print label of the input
+            Console.Write(inputLabel);
+            float inputValue = 0;
+
+            try {
+                inputValue = float.Parse(Console.ReadLine());
+
+            } catch (Exception ex) {
+                ConsoleWriteLineWithColor(inputErrorMsg, ConsoleColor.Red);
+                goto tryAgain;
+            }
+
+            return inputValue;
+        }
+
+        public double InputDouble(String inputLabel = "", string inputErrorMsg = "Your input is not in a correct format.") {
+        tryAgain:
+            // print label of the input
+            Console.Write(inputLabel);
+            double inputValue = 0;
+
+            try {
+                inputValue = float.Parse(Console.ReadLine());
+
+            } catch (Exception ex) {
+                ConsoleWriteLineWithColor(inputErrorMsg, ConsoleColor.Red);
+                goto tryAgain;
+            }
+
+            return inputValue;
+        }
+
+        public decimal InputDecimal(String inputLabel = "", string inputErrorMsg = "Your input is not in a correct format.") {
+        tryAgain:
+            // print label of the input
+            Console.Write(inputLabel);
+            decimal inputValue = 0;
+
+            try {
+                inputValue = Decimal.Parse(Console.ReadLine());
+
+            } catch (Exception ex) {
+                ConsoleWriteLineWithColor(inputErrorMsg, ConsoleColor.Red);
+                goto tryAgain;
+            }
+
+            return inputValue;
+        }
+
+        public string InputString(String inputLabel, bool lettersNumbersOnly = false) {
+        // print label of the input
+        start:
+            Console.Write(inputLabel);
+
+            string value = Console.ReadLine();
+
+            if (lettersNumbersOnly && !Regex.IsMatch(value, @"^[a-zA-Z0-9]+$")) {
+                ConsoleWriteLineWithColor("This input only accept alphabet characters, and integers.", ConsoleColor.Red);
+                goto start;
+
+            } else if (value == "") {
+                ConsoleWriteLineWithColor("This input cannot be empty.", ConsoleColor.Red);
+                goto start;
+
+            } else {
+                return value;
+            }
+        }
+
+        public void ConsoleWriteLineWithColor(string text = "This is default message.", ConsoleColor color = ConsoleColor.Gray) {
+            if (color != ConsoleColor.Gray)
+                Console.ForegroundColor = color;
+
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ Week4/Assignment4.1/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4._1
+{
+    class Program
+    {
+        private static Helper run;
+        static void Main(string[] args) {
+            run = new Helper();
+
+            run.ConsoleWriteLineWithColor("Assignment 4.1:", ConsoleColor.Blue);
+
+            MenuItem[] menuOptions = new MenuItem[5];
+
+            menuOptions[0] = new MenuItem("Part 1", part1);
+
+            menuOptions[1] = new MenuItem("Part 2", part2);
+
+            menuOptions[2] = new MenuItem("Part 3", part3);
+
+            menuOptions[3] = new MenuItem("Part 4", part4);
+
+            menuOptions[4] = new MenuItem("Exit", run.ExitProgram);
+
+            run.MainMenu(menuOptions, -1);
+        }
+
+        /* 1. Return even numbers
+         * Write a method that returns a string of even numbers greater than 0 and less than 100.
+         * Expected input and output
+         * ReturnEvenNumbers() → "2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98"
+        */
+        static void part1() {
+            run.ConsoleWriteLineWithColor("Assignment 4.1 Part 1: Write a method that returns a string of even numbers greater than 0 and less than 100.", ConsoleColor.Blue);
+
+        start:
+            int minInput = run.InputInteger("Minimum whole number: ");
+            int maxInput = run.InputInteger("Maximum whole number: ");
+
+            if (minInput > maxInput) {
+                run.ConsoleWriteLineWithColor("Your min number is greater than max number, please try again.", ConsoleColor.Red);
+                goto start;
+            }
+
+            run.ConsoleWriteLineWithColor("Result: "+ReturnEvenNumbers(minInput, maxInput), ConsoleColor.DarkCyan);
+        }
+
+        static string ReturnEvenNumbers(int min, int max) {
+            string result = "";
+
+            min = (min % 2 == 0) ? min + 2 : min + 1;
+
+            for (int i = min; i < max; i += 2)
+                result += i.ToString()+" ";
+
+            return result; 
+        }
+
+        /* 2. If year is leap
+         * Given a year as integer, write a method that checks if year is leap.
+         * Expected input and output
+         * IfYearIsLeap(2016) → true IfYearIsLeap(2018) → false
+         */
+        static public void part2() {
+            run.ConsoleWriteLineWithColor("Assignment 4.1 Part 2: Given a year as integer, write a method that checks if year is leap.", ConsoleColor.Blue);
+
+            int yearInput = run.InputInteger("Year: ");
+
+            // IfYearIsLeap(year) method is not used, since its redundant to use if statement again to change color 
+            if (DateTime.IsLeapYear(yearInput)) {
+                run.ConsoleWriteLineWithColor($"{yearInput} is leap year.", ConsoleColor.DarkGreen);
+            } else {
+                run.ConsoleWriteLineWithColor($"{yearInput} is NOT a leap year.", ConsoleColor.DarkRed);
+            }
+        }
+
+        static bool IfYearIsLeap(int year) {
+            if (DateTime.IsLeapYear(year))
+                return true;
+
+            return false;
+        }
+
+        /* 3. String in reverse order
+         * Given a string, write a method that returns that string in reverse order.
+         * Expected input and output
+         * StringInReverseOrder("qwerty") → "ytrewq" StringInReverseOrder("oe93 kr") → "rk 39eo"
+         */
+        static void part3() {
+            run.ConsoleWriteLineWithColor("Assignment 4.1 Part 3: Given a string, write a method that returns that string in reverse order.", ConsoleColor.Blue);
+
+            string userInput = run.InputString("Input a string: ");
+
+            run.ConsoleWriteLineWithColor($"String reversed: {StringInReverseOrder(userInput)}", ConsoleColor.DarkCyan);
+        }
+
+        static string StringInReverseOrder(string str) {
+            if (String.IsNullOrEmpty(str))
+                return "";
+
+            StringBuilder newStr = new StringBuilder();
+
+            for (int i = str.Length - 1; i >= 0; i--) {
+                newStr.Append(str[i]);
+            }
+
+            return newStr.ToString();
+        }
+
+        /* 4. Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+         * Test Data :
+         * Please input a string : This is a test string.
+         * Expected Output :
+         * "This is a test string." contains 4 spaces
+         */
+        static public void part4() {
+            run.ConsoleWriteLineWithColor("Assignment 4.1 Part 4: Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.", ConsoleColor.Blue);
+
+            string userInput = run.InputString("Write a sentence: ");
+
+            run.ConsoleWriteLineWithColor($"Total spaces in sentence: {CountStringSpaces(userInput)}", ConsoleColor.DarkCyan);
+        }
+
+        static int CountStringSpaces(string str) {
+            if (String.IsNullOrEmpty(str))
+                return 0;
+
+            int count = 0;
+
+            foreach (char c in str) {
+                if (c == ' ')
+                    count++; 
+            }
+
+            return count; 
+        }
+    }
+}
